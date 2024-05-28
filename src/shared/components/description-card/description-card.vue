032df654917275b2adcbde72d16658e4ff1b0738
Fernando Salgado <u202212023@upc.edu.pe>
2024-05-27 19:54:48 -0500

    feat(social-interaction): responsive view implemented for description card.

--- src/shared/components/description-card/description-card.vue
+++ src/shared/components/description-card/description-card.vue
@@ -3,47 +3,32 @@ export default { name: "description-card",}
 </script>
 
 <template>
-  <div class="cardD">
-    <h1 class="cardD-title">Descripción</h1>
-    <pv-card class="mt-1">
-      <template #content>
-        <ul>
-          <li><strong>Ubicación:</strong> Situada en Asia.</li>
-          <li><strong>Tipo de Granja:</strong> Avícola.</li>
-          <li><strong>Superficie Total:</strong> 50 hectáreas.</li>
-          <li><strong>Animales:</strong> Pollo</li>
-          <li><strong>Infraestructura:</strong> graneros, corrales, sistema de riego, y vivienda para los trabajadores.</li>
-          <li><strong>Servicios:</strong> Acceso a agua potable y electricidad.</li>
-          <li><strong>Estado:</strong> Bien mantenido, con todas las instalaciones en funcionamiento.</li>
-          <li><strong>Certificaciones:</strong> ISO 22000, BPA, HACCP</li>
-        </ul>
-      </template>
-    </pv-card>
-  </div>
+  <pv-card class="mt-1">
+    <template #content>
+      <ul>
+        <li><strong>Location:</strong> Located in Asia.</li>
+        <li><strong>Type of Farm:</strong> Poultry.</li>
+        <li><strong>Total Area:</strong> 50 hectares.</li>
+        <li><strong>Animals:</strong> Chicken</li>
+        <li><strong>Infrastructure:</strong> barns, pens, irrigation system, and housing for workers.</li>
+        <li><strong>Services:</strong> Access to drinking water and electricity.</li>
+        <li><strong>Status:</strong> Well maintained, with all facilities in operation.</li>
+        <li><strong>Certifications:</strong> ISO 22000, BPA, HACCP</li>
+      </ul>
+    </template>
+  </pv-card>
 </template>
 
 <style>
 
-.cardD{
-  position: relative;
-  display: flex;
-  justify-content: center;
-}
-
-.cardD-title {
-  position: absolute;
-  margin-top: 145px;
-  margin-left: 360px;
-  font-size: 25px;
-}
-
 .mt-1{
-  margin-top: 180px;
+  position:relative;
   width: 470px;
   height: 395px;
   border-radius: 10px;
   border: 1px solid gray;
-  margin-left: 400px;
+  margin-left: 10%;
+  margin-top: -4%;
 }
 
 ul {
